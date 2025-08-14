640f3e89303f186af917a65a6512c829397c7b90
kevin.juliandito <kevin.juliandito@vainsight.com>
2025-08-14 21:25:20 +0700

    Updated file format for counter page

--- src/views/BadmintonCounter.vue
+++ src/views/BadmintonCounter.vue
@@ -3,35 +3,57 @@
     <b-row class="justify-content-center align-items-center text-center h-100">
       <b-col md="5">
         <b-card no-body class="border-0 shadow-sm">
-            <b-card-body >
-                <b-row>
-                    <b-col>
-                        <h2 class="mb-4 font-weight-bold text-muted">Player 1 & Player 2</h2>
-                    </b-col>
-                </b-row>
-                <b-row>
-                    <b-col>
-                        <b-button @click="incrementScore('team1')" variant="success" size="lg" block class="mb-3 font-weight-bold w-100 d-flex justify-content-center align-items-center p-4">
-                            <b-icon-plus-circle-fill class="mx-2"></b-icon-plus-circle-fill> <span>Add Point</span>
-                        </b-button>
-                    </b-col>
-                </b-row>
-                <b-row>
-                    <b-col>
-                        <div class="d-flex justify-content-center score-display-container my-3">
-                            <div class="score-digit mx-2">{{ team1ScoreTens }}</div>
-                            <div class="score-digit mx-2">{{ team1ScoreOnes }}</div>
-                        </div>
-                    </b-col>
-                </b-row>
-                <b-row>
-                    <b-col>
-                        <b-button @click="decrementScore('team1')" variant="danger" size="lg" block class="mb-3 font-weight-bold w-100 d-flex justify-content-center align-items-center p-4">
-                            <b-icon-dash-circle-fill class="mx-2"></b-icon-dash-circle-fill> <span>Subtract Point</span>
-                        </b-button>
-                    </b-col>
-                </b-row>
-            </b-card-body>
+          <b-card-body>
+            <b-row>
+              <b-col>
+                <h2 class="mb-4 font-weight-bold text-muted">
+                  Player 1 & Player 2
+                </h2>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col>
+                <b-button
+                  @click="incrementScore('team1')"
+                  variant="success"
+                  size="lg"
+                  block
+                  class="mb-3 font-weight-bold w-100 d-flex justify-content-center align-items-center p-4"
+                >
+                  <b-icon-plus-circle-fill
+                    class="mx-2"
+                  ></b-icon-plus-circle-fill>
+                  <span>Add Point</span>
+                </b-button>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col>
+                <div
+                  class="d-flex justify-content-center score-display-container my-3"
+                >
+                  <div class="score-digit mx-2">{{ team1ScoreTens }}</div>
+                  <div class="score-digit mx-2">{{ team1ScoreOnes }}</div>
+                </div>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col>
+                <b-button
+                  @click="decrementScore('team1')"
+                  variant="danger"
+                  size="lg"
+                  block
+                  class="mb-3 font-weight-bold w-100 d-flex justify-content-center align-items-center p-4"
+                >
+                  <b-icon-dash-circle-fill
+                    class="mx-2"
+                  ></b-icon-dash-circle-fill>
+                  <span>Subtract Point</span>
+                </b-button>
+              </b-col>
+            </b-row>
+          </b-card-body>
         </b-card>
       </b-col>
 
@@ -43,22 +65,40 @@
         </b-row>
         <b-row>
           <b-col>
-            <b-button @click="startTimer" :disabled="timerRunning" variant="primary" block class="font-weight-bold w-100">
+            <b-button
+              @click="startTimer"
+              :disabled="timerRunning"
+              variant="primary"
+              block
+              class="font-weight-bold w-100"
+            >
               <b-icon-play-fill></b-icon-play-fill> Start
             </b-button>
           </b-col>
         </b-row>
         <b-row class="mt-2">
           <b-col>
-            <b-button @click="stopTimer" :disabled="!timerRunning" variant="warning" block class="font-weight-bold w-100">
+            <b-button
+              @click="stopTimer"
+              :disabled="!timerRunning"
+              variant="warning"
+              block
+              class="font-weight-bold w-100"
+            >
               <b-icon-stop-fill></b-icon-stop-fill> Stop
             </b-button>
           </b-col>
         </b-row>
         <b-row class="mt-2">
           <b-col>
-            <b-button @click="resetAll" variant="danger" block class="font-weight-bold w-100">
-              <b-icon-arrow-counterclockwise></b-icon-arrow-counterclockwise> Reset
+            <b-button
+              @click="resetAll"
+              variant="danger"
+              block
+              class="font-weight-bold w-100"
+            >
+              <b-icon-arrow-counterclockwise></b-icon-arrow-counterclockwise>
+              Reset
             </b-button>
           </b-col>
         </b-row>
@@ -66,38 +106,59 @@
 
       <b-col md="5">
         <b-card no-body class="border-0 shadow-sm">
-            <b-card-body >
-                <b-row>
-                    <b-col>
-                        <h2 class="mb-4 font-weight-bold text-muted">Player 3 & Player 4</h2>
-                    </b-col>
-                </b-row>
-                <b-row>
-                    <b-col>
-                        <b-button @click="incrementScore('team2')" variant="success" size="lg" block class="mb-3 font-weight-bold w-100 d-flex justify-content-center align-items-center p-4">
-                            <b-icon-plus-circle-fill class="mx-2"></b-icon-plus-circle-fill> <span>Add Point</span>
-                        </b-button>
-                    </b-col>
-                </b-row>
-                <b-row>
-                    <b-col>
-                        <div class="d-flex justify-content-center score-display-container my-3">
-                            <div class="score-digit mx-2">{{ team2ScoreTens }}</div>
-                            <div class="score-digit mx-2">{{ team2ScoreOnes }}</div>
-                        </div>
-                    </b-col>
-                </b-row>
-                <b-row>
-                    <b-col>
-                        <b-button @click="decrementScore('team2')" variant="danger" size="lg" block class="mb-3 font-weight-bold w-100 d-flex justify-content-center align-items-center p-4">
-                            <b-icon-dash-circle-fill class="mx-2"></b-icon-dash-circle-fill> <span>Subtract Point</span>
-                        </b-button>
-                    </b-col>
-                </b-row>
-            </b-card-body>
+          <b-card-body>
+            <b-row>
+              <b-col>
+                <h2 class="mb-4 font-weight-bold text-muted">
+                  Player 3 & Player 4
+                </h2>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col>
+                <b-button
+                  @click="incrementScore('team2')"
+                  variant="success"
+                  size="lg"
+                  block
+                  class="mb-3 font-weight-bold w-100 d-flex justify-content-center align-items-center p-4"
+                >
+                  <b-icon-plus-circle-fill
+                    class="mx-2"
+                  ></b-icon-plus-circle-fill>
+                  <span>Add Point</span>
+                </b-button>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col>
+                <div
+                  class="d-flex justify-content-center score-display-container my-3"
+                >
+                  <div class="score-digit mx-2">{{ team2ScoreTens }}</div>
+                  <div class="score-digit mx-2">{{ team2ScoreOnes }}</div>
+                </div>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col>
+                <b-button
+                  @click="decrementScore('team2')"
+                  variant="danger"
+                  size="lg"
+                  block
+                  class="mb-3 font-weight-bold w-100 d-flex justify-content-center align-items-center p-4"
+                >
+                  <b-icon-dash-circle-fill
+                    class="mx-2"
+                  ></b-icon-dash-circle-fill>
+                  <span>Subtract Point</span>
+                </b-button>
+              </b-col>
+            </b-row>
+          </b-card-body>
         </b-card>
       </b-col>
-
     </b-row>
   </b-container>
 </template>
@@ -108,17 +169,17 @@ import {
   BIconDashCircleFill,
   BIconPlayFill,
   BIconStopFill,
-  BIconArrowCounterclockwise
-} from 'bootstrap-vue';
+  BIconArrowCounterclockwise,
+} from "bootstrap-vue";
 
 export default {
-  name: 'BadmintonCounter',
+  name: "BadmintonCounter",
   components: {
     BIconPlusCircleFill,
     BIconDashCircleFill,
     BIconPlayFill,
     BIconStopFill,
-    BIconArrowCounterclockwise
+    BIconArrowCounterclockwise,
   },
   data() {
     return {
@@ -132,8 +193,10 @@ export default {
   computed: {
     // Computed property to format the timeElapsed into MM:SS format
     formattedTime() {
-      const minutes = Math.floor(this.timeElapsed / 60).toString().padStart(2, '0');
-      const seconds = (this.timeElapsed % 60).toString().padStart(2, '0');
+      const minutes = Math.floor(this.timeElapsed / 60)
+        .toString()
+        .padStart(2, "0");
+      const seconds = (this.timeElapsed % 60).toString().padStart(2, "0");
       return `${minutes}:${seconds}`;
     },
     // Computed properties to display the score in two separate digits
@@ -153,17 +216,17 @@ export default {
   methods: {
     // Method to increase the score for a given team
     incrementScore(team) {
-      if (team === 'team1') {
+      if (team === "team1") {
         this.team1Score++;
-      } else if (team === 'team2') {
+      } else if (team === "team2") {
         this.team2Score++;
       }
     },
     // Method to decrease the score, ensuring it doesn't go below 0
     decrementScore(team) {
-      if (team === 'team1' && this.team1Score > 0) {
+      if (team === "team1" && this.team1Score > 0) {
         this.team1Score--;
-      } else if (team === 'team2' && this.team2Score > 0) {
+      } else if (team === "team2" && this.team2Score > 0) {
         this.team2Score--;
       }
     },
@@ -194,7 +257,7 @@ export default {
   // Clean up the interval when the component is destroyed
   beforeDestroy() {
     clearInterval(this.timer);
-  }
+  },
 };
 </script>
 
@@ -208,7 +271,7 @@ export default {
 .score-digit {
   background-color: #e9ecef;
   border: 1px solid #ced4da;
-  border-radius: .5rem;
+  border-radius: 0.5rem;
   font-size: 15rem;
   font-weight: bold;
   width: 300px;
@@ -226,8 +289,8 @@ export default {
   color: #495057;
   background-color: #fff;
   padding: 10px 20px;
-  border-radius: .5rem;
-  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+  border-radius: 0.5rem;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   min-width: 150px;
   text-align: center;
 }
@@ -237,6 +300,6 @@ export default {
 }
 
 .font-weight-bold {
-    font-weight: 700 !important;
+  font-weight: 700 !important;
 }
 </style>
